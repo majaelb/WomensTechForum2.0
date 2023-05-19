2efe1ad1dddfe954f231a76fcd0cda2ed682b0a5
majaelb <115992540+majaelb@users.noreply.github.com>
2023-05-19 10:02:14 +0200

    Add correct send-form in message

--- WomensTechForum2.0/Pages/Message.cshtml.cs
+++ WomensTechForum2.0/Pages/Message.cshtml.cs
@@ -1,17 +1,35 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WomensTechForum2._0.Areas.Identity.Data;
 using WomensTechForum2._0.Models;
 
 namespace WomensTechForum2._0.Pages
 {
     public class MessageModel : PageModel
     {
+        public UserManager<WomensTechForum2_0User> _userManager;
+
         [BindProperty]
         public Message NewMessage { get; set; }
-
         public List<Message> Messages { get; set; }
+        public List<WomensTechForum2_0User> Users { get; set; }
+        public WomensTechForum2_0User CurrentUser { get; set; }
+
+        public MessageModel(UserManager<WomensTechForum2_0User> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public async Task<IActionResult> OnGetAsync()
         {
+            ViewData["ReceiverId"] = new SelectList(_userManager.Users, "Id", "FirstName");
+
+
+            Users = await _userManager.Users.ToListAsync();
+            CurrentUser = await _userManager.GetUserAsync(User);
             Messages = await DAL.MessageManager.GetAllMessages();
 
             return Page();
@@ -25,7 +43,7 @@ namespace WomensTechForum2._0.Pages
                 await DAL.MessageManager.SaveMessage(NewMessage);
             }
             Messages = await DAL.MessageManager.GetAllMessages();
-            return Page();
+            return RedirectToPage("./Message");
 
         }
     }
